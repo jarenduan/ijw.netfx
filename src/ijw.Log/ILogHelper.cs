1ab734b2569c38a7e3f5c203470f925bbe9c6240
jarenduan <jarenduan@gmail.com>
2016-07-02 15:39:37 +0800

    adding ijw.log

---
+++ src/ijw.Log/ILogHelper.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ijw.Log {
+    public interface ILogHelper {
+        void WriteError(string msg);
+        void WriteError(Exception ex);
+        void WriteInfo(string msg);
+        void WriteInfo(Exception ex);
+    }
+}
