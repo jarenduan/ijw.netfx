95d475ca0aad10f4d74f2cc0561e8f02b1e58177
jarenduan <jarenduan@gmail.com>
2016-07-02 15:32:05 +0800

    add reflection text

---
+++ src/ijw/Reflection/ReflectionHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace ijw.Reflection {
+    /// <summary>
+    /// 反射功能帮助类
+    /// </summary>
+    public static class ReflectionHelper {
+        /// <summary>
+        /// 获取属性的名称
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="expr">实例属性的表达式, 如foo => foo.bar </param>
+        /// <returns></returns>
+        public static string GetPropertyName<T>(Expression<Func<T, object>> expr) {
+            var rtn = "";
+            if (expr.Body is UnaryExpression) {
+                rtn = ((MemberExpression)((UnaryExpression)expr.Body).Operand).Member.Name;
+            }
+            else if (expr.Body is MemberExpression) {
+                rtn = ((MemberExpression)expr.Body).Member.Name;
+            }
+            else if (expr.Body is ParameterExpression) {
+                rtn = ((ParameterExpression)expr.Body).Type.Name;
+            }
+            return rtn;
+        }
+    }
+}
---
+++ src/ijw/Text/EncodingHelper.cs
@@ -0,0 +1,10 @@
+﻿using System.Text;
+
+namespace ijw.Text {
+    public static class EncodingHelper {
+        /// <summary>
+        /// GB2312 编码
+        /// </summary>
+        public static Encoding GB2312 => Encoding.GetEncoding("GB2312");
+    }
+}
