209fb24027b44462632cfcf0d4ebc16b999749ef
jarenduan <jarenduan@gmail.com>
2016-07-03 20:42:42 +0800

    add appconfig.cs

---
+++ ijw.Client/AppConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+
+namespace ijw.dotnet.Client {
+    public class AppConfig {
+        private Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+        public string this[string settingName]
+        {
+            get
+            {
+                var setting = config.AppSettings.Settings[settingName];
+                if (setting == null)
+                    return null;
+                else
+                    return setting.Value;
+            }
+            set
+            {
+                var setting = config.AppSettings.Settings[settingName];
+                if (setting == null)
+                    throw new NullReferenceException();
+                else
+                    setting.Value = value;
+            }
+        }
+    }
+}
