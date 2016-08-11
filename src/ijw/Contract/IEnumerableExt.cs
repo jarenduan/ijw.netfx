f9a40a6c81d195a0691b06f3a1d8d6e6d7afb2fb
jarenduan <jarenduan@gmail.com>
2016-08-11 11:30:02 +0800

    comment

--- src/ijw/Contract/IEnumerableExt.cs
+++ src/ijw/Contract/IEnumerableExt.cs
@@ -4,11 +4,14 @@ using System.Linq;
 
 namespace ijw.Contract {
     public static class IEnumerableExt {
-        public static bool ShouldNotBeNullOrEmpty<T>(this IEnumerable<T> collection) {
+        public static bool ShouldNotBeEmpty<T>(this IEnumerable<T> collection) {
+            collection.ShouldBeNotNullReference();
             return collection.Count().ShouldBeNotZero();
         }
-
-
+        public static bool ShouldNotBeNullOrEmpty<T>(this IEnumerable<T> collection) {
+            collection.ShouldBeNotNullReference();
+            return collection.ShouldNotBeEmpty();
+        }
         public static bool ShouldEachSatisfy<T>(this IEnumerable<T> collection, Predicate<T> condition) {
             foreach (var item in collection) {
                 item.ShouldSatisfy(condition);
