06eb7df61f17eb3aae065cdf093abb1acd13b321
jarenduan <jarenduan@gmail.com>
2016-09-03 01:03:24 +0800

    change objecttcpXXX to cachedTcpXXX

---
+++ src/ijw.Net.Socket/CachedTcpClient.cs
@@ -0,0 +1,208 @@
+﻿using ijw.Collection;
+using ijw.Diagnostic;
+using ijw.IO;
+using ijw.Log;
+using ijw.Threading.Tasks;
+using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ijw.Net.Socket {
+    /// <summary>
+    /// CachedTcpClient&lt;T&gt;实现了一个带有内部缓存的TCP数据发送客户端. 
+    /// </summary>
+    /// <typeparam name="T">发送数据的类型</typeparam>
+    /// <remarks>
+    /// 内部维护了数据缓冲池.1)负责向池中追加对象. 2)根据既定策略取出一条数据进行发送 3)成功发送后从池中删除该对象.
+    /// </remarks>
+    public class CachedTcpClient<T> {
+        /// <summary>
+        /// 服务器端的端口号
+        /// </summary>
+        public int PortNum { get; set; }
+
+        /// <summary>
+        /// 服务器的IP地址
+        /// </summary>
+        public string HostName { get; set; }
+
+        /// <summary>
+        /// 发送某个对象遇到错误时的最大尝试次数
+        /// </summary>
+        public int MaxRetryTimes { get; set; } = 5;
+
+        /// <summary>
+        /// 成功发送一个对象后会调用此委托
+        /// </summary>
+        public Action<T> ItemsSentAction { get; set; }
+
+        /// <summary>
+        /// Item数量发生变更时会调用此委托.
+        /// </summary>
+        public Action<int> ItemCountChangedAction { get; set; }
+
+        /// <summary>
+        /// 负责完成将一个Item写入网络流中，并关闭流。默认实现是向流中写入item的ToString()。
+        /// </summary>
+        public Action<NetworkStream, T> WriteItemAndDisposeAction = (s, i) => s.WriteStringAndDispose(i.ToString());
+
+        /// <summary>
+        /// 构造一个对象发送客户端
+        /// </summary>
+        /// <param name="getStratrgy">获取对象的策略</param>
+        /// <param name="hostName">主机IP, 默认 127.0.0.1</param>
+        /// <param name="portNum">端口号, 默认15210(obj三个字母的序号)</param>
+        /// <param name="logOn">是否开启日志</param>
+        public CachedTcpClient(FetchStrategies getStratrgy, string hostName = "127.0.0.1", int portNum = 15210, bool logOn = true) {
+            this._dataPool.ItemGettngStrategy = getStratrgy;
+            this.HostName = hostName;
+            this.PortNum = portNum;
+            this._dataPool.ItemCountChanged += (o, e) => {
+                this._ItemsCountChanged.Report(e.ItemCount);
+            };
+            this._ItemsCountChanged = new Progress<int>((count) => {
+                this.ItemCountChangedAction?.Invoke(count);
+            });
+            this._ItemSent = new Progress<T>((obj) => {
+                this.ItemsSentAction?.Invoke(obj);
+            });
+            if (logOn) {
+                this._logger = new LogHelper();
+            }
+        }
+
+        /// <summary>
+        /// 追加数据项到缓存中
+        /// </summary>
+        /// <param name="item">要放入缓存的数据项</param>
+        /// <param name="isResume">是否立即恢复发送</param>
+        public void AppendingItem(T item, bool isResume = false) {
+            Task.Run(() => {
+                DebugHelper.WriteLine("Try to append item.");
+                _dataPool.Append(item);
+                if (isResume) {
+                    ResumeSending();
+                }
+            });
+        }
+
+        /// <summary>
+        /// 启动发送. 将在后台的工作线程中循环执行.
+        /// 每次迭代时将会检查StopCondition, 如果为true, 将退出循环, 发送操作结束.
+        /// 每次迭代时如果数据池中有数据, 将会进行TCP发送, 发送后会从数据池中移除该数据. 
+        /// 如果没有数据, 迭代将暂停, 等待通知信号后继续进行.
+        /// </summary>
+        public async Task StartSendingAsync() {
+            _bgLoopwork = new BackgroundLooper();
+            _bgLoopwork.StopCondition = () => false;
+            _bgLoopwork.WaitCondition = () => !this._dataPool.HasItem;
+            _bgLoopwork.LoopAction = () => {
+                //if(this.DataPool.Count >= 512) {
+                //    BatchHandle();
+                //}
+                T item;
+                if (_dataPool.TryBorrow(out item)) {
+                    DebugHelper.WriteLine("Start sending item...");
+                    int i = 1;
+                    while (i < MaxRetryTimes) {
+                        if (i > 1) {
+                            DebugHelper.WriteLine("Sending error detected. Wait 1 second to try again.");
+                            Thread.Sleep(1000);
+                            DebugHelper.WriteLine("Try sending again...");
+                        }
+
+                        if (sendData(item)) {
+                            DebugHelper.WriteLine("Item Sent. Try removing item from pool.");
+                            _dataPool.Remove(item);
+                            _ItemSent.Report(item);
+                            _logger?.WriteInfo(item.ToString() + " is sent");
+                            return;
+                        }
+                        i++;
+                    }
+                    _dataPool.Return(item);
+                    throw new Exception(string.Format("Sending fail: has retry {0} times. 有可能服务器未开启或者网络问题.", MaxRetryTimes));
+                }
+            };
+            await _bgLoopwork.StartAsync();
+        }
+
+        /// <summary>
+        /// 暂停发送，可用<see cref="ResumeSending"/>恢复。
+        /// </summary>
+        public void SuspendSending() {
+            this._bgLoopwork.Suspend();
+        }
+
+        /// <summary>
+        /// 恢复发送.
+        /// </summary>
+        public void ResumeSending() {
+            DebugHelper.WriteLine("Wake up the loop.");
+            this._bgLoopwork.Resume();
+        }
+
+        /// <summary>
+        /// 停止发送
+        /// </summary>
+        public void StopSending() {
+            this._bgLoopwork.Exit();
+        }
+
+        //TODO: change to await
+        private bool sendData(T item) {
+            TcpClient client = null;
+            try {
+                client = new TcpClient(HostName, PortNum);
+                Thread.Sleep(_CLIENT_CONNECTION_TIME_WAIT);
+            }
+#pragma warning disable 168
+            catch {
+                close(client);
+                DebugHelper.WriteLine("无法连接, 可能是网络问题/服务器没有开启/地址端口不对!");
+                _logger?.WriteError("无法连接, 可能是网络问题/服务器没有开启/地址端口不对!");
+                return false;
+            }
+#pragma warning restore 168
+            DebugHelper.WriteLine("Tcp connected.");
+            try {
+                using (NetworkStream ns = client.GetStream()) { 
+                    this.WriteItemAndDisposeAction?.Invoke(ns, item);
+                }
+                _logger?.WriteInfo("数据发送成功:" + item.ToString());
+            }
+#pragma warning disable 168
+            catch {
+                close(client);
+                _logger?.WriteError("数据发送失败.");
+                return false;
+            }
+#pragma warning restore 168
+            close(client);
+            return true;
+        }
+
+        /// <summary>
+        /// 批处理之前的剩余数据
+        /// </summary>
+        private void BatchHandle() {
+            //TODO: implement;
+            throw new NotImplementedException();
+        }
+
+        private void close(TcpClient client) {
+            if (client != null) {
+                client.Close();
+                DebugHelper.WriteLine("Tcp closed.");
+            }
+        }
+
+        private const int _CLIENT_CONNECTION_TIME_WAIT = 10;
+        private LongTimeConsumerCollection<T> _dataPool = new LongTimeConsumerCollection<T>();
+        private BackgroundLooper _bgLoopwork = new BackgroundLooper();
+        private LogHelper _logger;
+        private IProgress<int> _ItemsCountChanged;
+        private IProgress<T> _ItemSent;
+    }
+}
--- src/ijw.Net.Socket/CachedTcpServer.cs
+++ src/ijw.Net.Socket/CachedTcpServer.cs
@@ -1,18 +1,16 @@
-﻿//TODO: support netstandard when netcore support binaryserialization
+﻿using ijw.Collection;
+using ijw.Diagnostic;
 using ijw.Log;
-using ijw.Collection;
-using ijw.Serialization.Binary;
 using System;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 using System.Threading.Tasks;
-using ijw.Diagnostic;
 
 namespace ijw.Net.Socket {
     /// <summary>
-    /// ObjectTCPServer&lt;T&gt; 基于TCP协议, 通过监听指定端口, 实现了一个泛型对象远程接收/通知处理服务器. 
-    /// 调用者可通过指定ObjectRecievedHandlerAsync委托, 方便地以异步的方式处理新接收到的对象.
+    /// CachedTcpServer&lt;T&gt;实现了一个泛型对象TCP接收/通知处理服务器. 
+    /// 调用者可通过指定ItemRecievedHandlerAsync委托, 方便地以异步的方式处理新接收到的对象.
     /// 调用者也可以通过挂接事件处理器, 方便地同步处理接收到的对象.
     /// </summary>
     /// <typeparam name="T">获取对象的类型</typeparam>
@@ -20,17 +18,17 @@ namespace ijw.Net.Socket {
     /// 调用StartAsync()方法, 内部启动了两个Task. 一个是负责监听TCP端口. 另一个负责发出通知事件.
     /// 两个Task共同维护了内部的一个线程安全的泛型集合. 
     /// </remarks>
-    public class ObjectTCPServer<T> : IDisposable {
+    public class CachedTcpServer<T> : IDisposable where T: class {
         /// <summary>
         /// 初始化服务器实例
         /// </summary>
-        /// <param name="ifSupportUIThreading">是否对UI线程操作提供支持. true 则每次接收到对象将会封送至ObjectHandlerAsync所在线程进而可以更改控件. 反之ObjectHandlerAsync将在后台线程运行, 从而无法更改UI线程中的控件.</param>
-        public ObjectTCPServer(bool ifSupportUIThreading = false) {
+        /// <param name="ifSupportUIThreading">是否对UI线程操作提供支持. true 则每次接收到对象将会封送至ItemHandlerAsync所在线程进而可以更改控件. </param>
+        public CachedTcpServer(bool ifSupportUIThreading = false) {
             this._withUIThreadingSupport = ifSupportUIThreading;
             if (this._withUIThreadingSupport) {
                 //注: 此种方式调用就可以进行异步操作了, 弥补了同步事件的不足
-                this._objectAppendedProgess = new Progress<T>(async (o) => {
-                    await invokeObjectRecievedAsync(o);
+                this._itemAppendedProgess = new Progress<T>(async (o) => {
+                    await invokeItemRecievedAsync(o);
                 });
             }
         }
@@ -51,17 +49,12 @@ namespace ijw.Net.Socket {
         /// <summary>
         /// 监听的端口
         /// </summary>
-        public int PortNum { get; set; }
+        public int PortNum { get; set; } 
 
         /// <summary>
         /// 服务器是否停止监听
         /// </summary>
-        public bool IsListenerRunning {
-            get {
-                return this._isListenerRunning;
-            }
-        }
-        //TDDO: remove cts and ct!
+        public bool IsListenerRunning => this._isListenerRunning;  
         #endregion
 
         #region Events
@@ -69,7 +62,12 @@ namespace ijw.Net.Socket {
         /// 接收到对象后进行调用的委托, 此委托将会异步执行.
         /// 注意: 即使调用Stop方法停止了接收, 但是如果对象池仍有对象没有被处理, 此委托仍会被调用, 直至所有对象被成功处理.
         /// </summary>
-        public Func<T, Task> ObjectHandlerAsync { get; set; }
+        public Func<T, Task> ItemRecievedHandlerAsync { get; set; }
+
+        /// <summary>
+        /// 接收到流后，从流中解析取回数据项，并关闭流. 没有任何数据返回空. 有数据但解析发生错误应该抛出异常.
+        /// </summary>
+        public Func<NetworkStream, T> RetrieveItemAndDispose { get; set; }
 
         /// <summary>
         /// 服务器停止后, 且全部对象被成功处理后, 激活此事件.
@@ -77,49 +75,35 @@ namespace ijw.Net.Socket {
         public event EventHandler AllItemsHandled;
 
         /// <summary>
-        /// 当有数据接受到, 追加到数据池中, 会触发此事件. EventArg 是对象池的引用.
-        /// </summary>
-        /// <remarks>
-        /// 一般地, 事件处理器应调用集合的TryGetItem方法取出下一个需要处理的对象, 成功处理后, 应该调用Remove方法将其移出对象池.
-        /// 如果处理失败, 应该调用Return方法还回对象池. 对象集合是线程安全的.
-        /// 另外, 此事件将会运行在通知任务的线程中, 并不会阻塞端口监听.
-        /// 但此事件是同步调用, 因此会导致诸如无法更改前台界面线程中的窗体控件等线程同步问题.
-        /// </remarks>
-        public event EventHandler<ObjectAvailabeEventArgs<T>> ObjectAvailable;
-
-        /// <summary>
         /// 接收到损坏对象
         /// </summary>
-        public event EventHandler BadObjectRecieved;
+        public event EventHandler BadItemRecieved;
         #endregion
-        
+
         #region Start & Stop
         /// <summary>
         /// 启动服务器
         /// 此操作将在内部启动两个Task. 分别负责端口监听和事件激发.
         /// 启动监听后, 代码会立即返回. 
-        /// 可通过注册<see cref="ObjectAvailable"/>事件来处理接收到的对象.
+        /// 可通过注册<see cref="ItemRecieved"/>事件来处理接收到的对象.
         /// </summary>
         /// <remarks>
         /// 未提供此方法的同步版本.
         /// 以await方式调用, 可用TryCatch捕捉内部Task中的异常.
         /// </remarks>
         public async Task StartAsync() {
-            if(this._isListenerRunning) {
-                DebugHelper.WriteLine("[Main] Server is running.");
-                logger.WriteError("[Main] Server is running.");
+            if (this._isListenerRunning) {
+                DebugHelper.WriteLine("[Main] Server is already running.");
+                _logger.WriteError("[Main] Server is already running.");
                 return;
             }
             startOrResumeNotifyLoop();
             try {
                 await startNewListenerLoopAsync();
             }
-            catch(Exception ex) {
-                throw ex;
-            }
             finally {
-                if(this._isListenerRunning) {
-                    this.Stop();
+                if (this._isListenerRunning) {
+                    this.StopListening();
                 }
             }
         }
@@ -128,15 +112,14 @@ namespace ijw.Net.Socket {
         /// 停止服务器.
         /// 注意: Stop方法只停止了监听线程. 内部的通知处理线程仍会继续循环, 直至所有对象都被处理和移出集合.
         /// </summary>
-        public void Stop() {
+        public void StopListening() {
             DebugHelper.WriteLine("[Listener] Got STOP signal.");
-            if(!this._isListenerRunning) {
+            if (!this._isListenerRunning) {
                 DebugHelper.WriteLine("[Listener] Listener is not running.");
                 return;
             }
             //发出停止信号
             stopListening();
-            
         }
         #endregion
 
@@ -155,18 +138,19 @@ namespace ijw.Net.Socket {
                 });
                 closeListenerIfThereIs();
             }
-            catch(Exception e) {
+            catch (Exception e) {
                 DebugHelper.WriteLine(e.ToString());
                 throw e;
             }
             finally {
                 closeClientIfThereIs();
-                if(this._isListenerRunning) {
+                if (this._isListenerRunning) {
                     closeListenerIfThereIs();
                 }
                 this._isListenerRunning = false;
             }
         }
+
         /// <summary>
         /// 循环监听TcpClient, 并从中读取对象, 直至用户取消监听.
         /// 一旦读到对象, 将会向内部数据池中追加对象, 并send信号有数据追加.
@@ -176,16 +160,16 @@ namespace ijw.Net.Socket {
             this._isListenerRunning = true;
             this._shouldContinueListen = true;
             //开始循环
-            while(this._shouldContinueListen) {
+            while (this._shouldContinueListen) {
                 waitingForTCPClient();
                 try {
-                    if(retrieve_and_append_object()) {
+                    if (retrieve_and_append_item()) {
                         wake_notifyLoop();
                     }
                 }
                 catch {
-                    DebugHelper.WriteLine("[Listener] Bad object, dropped. Or stop signal ");
-                    this.BadObjectRecieved.InvokeIfNotNull(this, null);
+                    DebugHelper.WriteLine("[Listener] Bad item, dropped. Or stop signal ");
+                    this.BadItemRecieved.InvokeIfNotNull(this, null);
                 }
                 finally {
                     closeClientIfThereIs();
@@ -197,12 +181,13 @@ namespace ijw.Net.Socket {
             this._shouldContinueNotify = false; //通知线程 => 应该停止
             wake_notifyLoop();
         }
+
         /// <summary>
         /// 发出停止监听的信号
         /// </summary>
         private void stopListening() {
             DebugHelper.WriteLine("[Listener] Try to stop...");
-            lock(this._notifierSyncRoot) {
+            lock (this._notifierSyncRoot) {
                 //设置监听令牌
                 this._shouldContinueListen = false;
                 //设置通知令牌
@@ -223,14 +208,14 @@ namespace ijw.Net.Socket {
             DebugHelper.WriteLine("[Listener] New listener Started.");
         }
         private void closeListenerIfThereIs() {
-            if(this._listener != null) {
+            if (this._listener != null) {
                 this._listener.Stop();
                 DebugHelper.WriteLine("[Main] Listener stopped.");
                 this._listener = null;
             }
         }
         private void closeClientIfThereIs() {
-            if(this._client != null) {
+            if (this._client != null) {
                 this._client.Close();
                 DebugHelper.WriteLine("[Listener] TCP Connection closed.");
                 this._client = null;
@@ -248,8 +233,7 @@ namespace ijw.Net.Socket {
             catch {
             }
             finally {
-                if(client != null)
-                    client.Close();
+                client?.Close();
             }
         }
         /// <summary>
@@ -267,12 +251,12 @@ namespace ijw.Net.Socket {
         /// 启动通知线程
         /// </summary>
         private void startOrResumeNotifyLoop() {
-            lock(this._notifierSyncRoot) {
+            lock (this._notifierSyncRoot) {
                 if (this._isNotifierRunning) {
                     //notifyLoop在运行, 通知应该继续循环.
                     DebugHelper.WriteLine("[Main] Current notifier should continue.");
                     //stopListening方法有可能将_shouldContinueNotify设为false. 重设为true会保证notifyLoop即使在最后时刻也可以恢复循环.
-                    this._shouldContinueNotify = true; 
+                    this._shouldContinueNotify = true;
                 }
                 else {
                     //如果notifyloop没在运行, 开始新线程进行新的loop.
@@ -302,19 +286,18 @@ namespace ijw.Net.Socket {
             this._isNotifierRunning = true;
             this._shouldContinueNotify = true;
             //开始循环
-            while(true) {
+            while (true) {
                 //如果发现对象池中有对象需要处理:
-                if(this._objectPool.HasNonConsumingItem) { 
-                    DebugHelper.WriteLine("[Notifier] Found non-consuming object(s).");
-                    T obj;
-                    if (this._objectPool.TryGetItemNotConsuming(out obj)) {
-                        invokeAsyncObjectHandlerIfPossible(obj);
-                        invokeObjectHandlerIfPossible();
+                if (this._dataPool.IsItemAvailable) {
+                    DebugHelper.WriteLine("[Notifier] Found non-consuming item.");
+                    T item;
+                    if (this._dataPool.TryBorrowAvailable(out item)) {
+                        invokeAsyncItemHandlerIfPossible(item);
                     }
                     continue; //处理完, 进入下次迭代.
                 }
                 //没有对象需要处理, 用户没终止监听:
-                if(this._shouldContinueNotify) {
+                if (this._shouldContinueNotify) {
                     DebugHelper.WriteLine("[Notifier] Sleeping...");
                     sleepUntilNextSignal(); //进入睡眠状态.
                     DebugHelper.WriteLine("[Notifier] Wake up.");
@@ -322,9 +305,9 @@ namespace ijw.Net.Socket {
                 }
                 //没有对象需要处理, 线程不需要继续:
                 DebugHelper.WriteLine("[Notifier] Should stop. ");
-                if(this._objectPool.HasItem) {
+                if (this._dataPool.HasItem) {
                     //但是还存在一些对象, 都在处理中
-                    System.Threading.Thread.Sleep(500); //等一会
+                    Thread.Sleep(500); //等一会
                     continue; //再迭代一次.
                 }
                 //所有对象都被处理了, 对象池中没对象了
@@ -332,11 +315,11 @@ namespace ijw.Net.Socket {
                 this.AllItemsHandled.InvokeIfNotNull(this, null);
                 //即将结束循环
                 DebugHelper.WriteLine("[Notifier] Should stop notify loop");
-                lock(this._notifierSyncRoot) {
+                lock (this._notifierSyncRoot) {
                     //锁住状态, 最后检查一下是否应该继续
                     DebugHelper.Write("[Notifier] Last check...");
                     //用户指示需要继续.
-                    if(this._shouldContinueNotify) {
+                    if (this._shouldContinueNotify) {
                         DebugHelper.WriteLine("[Notifier] Resumed.");
                         continue; //循环需要继续, 重新进入迭代.
                     }
@@ -348,72 +331,71 @@ namespace ijw.Net.Socket {
             this._isNotifierRunning = false;
             DebugHelper.WriteLine("[Notifier] Stopped.");
         }
+
         /// <summary>
-        /// 尝试invoke同步进度报告事件
+        /// 尝试激活异步进度处理委托
         /// </summary>
-        private void invokeObjectHandlerIfPossible() {
-            DebugHelper.WriteLine("Try to Invoke ObjectAppended event.");
-            this.ObjectAvailable.InvokeIfNotNull(this, null);
-        }
-        /// <summary>
-        /// 尝试invoke异步进度处理委托
-        /// </summary>
-        /// <param name="obj"></param>
-        private void invokeAsyncObjectHandlerIfPossible(T obj) {
-            //_objectAppendedReport总不为null, 因此只需要检查ObjectRecievedHandlerAsync即可.
-            if(this.ObjectHandlerAsync != null) {
+        /// <param name="item">需要处理的数据</param>
+        private void invokeAsyncItemHandlerIfPossible(T item) {
+            //_itemAppendedReport总不为null, 因此只需要检查ItemRecievedHandlerAsync即可.
+            if (this.ItemRecievedHandlerAsync != null) {
                 //支持界面控件线程的话, 就使用Progress.Report
-                if(this._withUIThreadingSupport) {
-                    _objectAppendedProgess.Report(obj);
+                if (this._withUIThreadingSupport) {
+                    _itemAppendedProgess.Report(item);
                 }
                 //不需要UI线程支持的话, 就直接调用注册的委托
                 else {
 #pragma  warning disable 4014
-                    invokeObjectRecievedAsync(obj);
+                    invokeItemRecievedAsync(item);
 #pragma  warning restore 4014
                 }
             }
         }
+
+        /// <summary>
+        /// 取出对象, 交由ItemRecievedHandlerAsync 委托处理, 之后会移除对象. 
+        /// ItemRecievedHandlerAsync 委托抛出异常的话则将对象还回对象池, 供下次使用
+        /// </summary>
+        /// <param name="item">取出的对象</param>
+        /// <returns></returns>
+        private async Task invokeItemRecievedAsync(T item) {
+            try {
+                DebugHelper.WriteLine("Invoke ItemRecievedHandlerAsync asyncly.");
+                await this.ItemRecievedHandlerAsync(item);
+                _dataPool.Remove(item);
+                _logger.WriteInfo("Item handled successfully.");
+            }
+            catch (Exception ex) {
+                DebugHelper.WriteLine($"{nameof(ItemRecievedHandlerAsync)} exception. Should return item.");
+                _dataPool.Return(item);
+                wake_notifyLoop(); //returned, so there is item available again, notify.
+                _logger.WriteError(ex.Message);
+                throw ex;
+            }
+        }
+
         /// <summary>
         /// 尝试获取并解析到对象
         /// </summary>
         /// <returns>成功解析到对象, 返回true; 反之返回false.</returns>
-        private bool retrieve_and_append_object() {
+        private bool retrieve_and_append_item() {
             var networkStream = _client.GetStream();
-            DebugHelper.WriteLine("[Listener] Try to retrieve object...");
-            var obj = networkStream.RetrieveBinaryObjectAndDispose<T>();
+            DebugHelper.WriteLine("[Listener] Try to retrieve item...");
+            T item = null;
+            using (networkStream) {
+                item = this.RetrieveItemAndDispose?.Invoke(networkStream);
+            }
             //有可能是此类自己发出的唤醒client的假数据, 那样什么也不会解析到
-            if(obj != null) {
-                this._objectPool.Append(obj);
-                DebugHelper.WriteLine("[Listener] An object appended.");
+            if (item != null) {
+                this._dataPool.Append(item);
+                DebugHelper.WriteLine("[Listener] A data item appended.");
             }
             else {
-                DebugHelper.WriteLine("[Listener] No object retrieved.");
-                //Console.WriteLine("No object retrieved!");
+                DebugHelper.WriteLine("[Listener] No data item retrieved.");
             }
-            return obj != null;
+            return item != null;
         }
-        /// <summary>
-        /// 取出对象, 交由ObjectRecievedHandlerAsync委托处理, 之后会移除对象. 
-        /// ObjectRecievedHandlerAsync委托抛出异常的话则将对象还回对象池, 供下次使用
-        /// </summary>
-        /// <param name="obj">取出的对象</param>
-        /// <returns></returns>
-        private async Task invokeObjectRecievedAsync(T obj) {
-            try {
-                DebugHelper.WriteLine("Invoke ObjectRecievedHandlerAsync delegate asyncly.");
-                await this.ObjectHandlerAsync(obj);
-                _objectPool.Remove(obj);
-                logger.WriteInfo("Object handled successfully.");
-            }
-            catch (Exception ex) {
-                DebugHelper.WriteLine("ObjectRecievedHandlerAsync exception. Should return object.");
-                _objectPool.Return(obj);
-                wake_notifyLoop();
-                logger.WriteError(ex.Message);
-                throw ex;
-            }
-        }
+
         #endregion
 
         #region Control between the listen and notify jobs
@@ -427,7 +409,7 @@ namespace ijw.Net.Socket {
             this._are.Set();
         }
 
-        
+
         #endregion
 
         #endregion
@@ -452,11 +434,11 @@ namespace ijw.Net.Socket {
         /// <summary>
         /// 进度通知委托
         /// </summary>
-        protected IProgress<T> _objectAppendedProgess;
+        protected IProgress<T> _itemAppendedProgess;
         /// <summary>
         /// 接受到的对象池
         /// </summary>
-        protected LongTimeConsumerCollection<T> _objectPool = new LongTimeConsumerCollection<T>();
+        protected LongTimeConsumerCollection<T> _dataPool = new LongTimeConsumerCollection<T>();
         /// <summary>
         /// 通知线程是否在运行
         /// </summary>
@@ -484,7 +466,7 @@ namespace ijw.Net.Socket {
         /// <summary>
         /// 日志记录器
         /// </summary>
-        protected LogHelper logger = new LogHelper();
+        protected LogHelper _logger = new LogHelper();
         #endregion
     }
 }
