12da8494af2194e75d8a6c4e06156f354eb2921b
jarenduan <jarenduan@gmail.com>
2016-09-03 00:50:05 +0800

    add tobytes extensions

---
+++ src/ijw.Core/Text/StringExt.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ijw.Text {
+    public static class StringExt {
+        public static byte[] ToBytes(this string aString, Encoding encoding) {
+            return encoding.GetBytes(aString);
+        }
+
+        /// <summary>
+        /// 使用UTF8编码转换成字节数组
+        /// </summary>
+        /// <param name="aString"></param>
+        /// <returns></returns>
+        public static byte[] ToBytes(this string aString) {
+            return aString.ToBytes(Encoding.Unicode);
+        }
+
+
+    }
+}
