fcdec7b75e8ab3e2ffb0fe5cb1b93afd29bf4bd1
jarenduan <jarenduan@gmail.com>
2016-09-05 15:13:57 +0800

    add shouldbeValidPortNumber contract

---
+++ src/ijw.Net.Socket/Contract/IntegerExt.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ijw.Contract;
+
+namespace ijw.Net.Socket {
+    public static class IntegerExt {
+        public static bool ShouldBeValidPortNumber(this int num) {
+            return num.ShouldNotLargerThan(65535) && num.ShouldBeNotLessThanZero();
+        }
+    }
+}
