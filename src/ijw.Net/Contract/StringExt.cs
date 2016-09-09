b94506c2f5a4e70f15860a9c812e69400452534a
jarenduan <jarenduan@gmail.com>
2016-09-09 19:51:29 +0800

    add ipv4 string contract

---
+++ src/ijw.Net/Contract/StringExt.cs
@@ -0,0 +1,15 @@
+﻿using ijw.Contract;
+
+namespace ijw.Net.Contracts {
+    public static class StringExt {
+        public static bool ShouldBeIPv4Address(this string aString) {
+            var r = aString.IsIPv4Address();
+            if (!r) {
+                return false;
+            }
+            else {
+                throw new ContractBreakException($"{aString} is not a IPv4 Address");
+            }
+        }
+    }
+}
