7b955949cbba21b46662f791086c21b94143495d
jarenduan <jarenduan@gmail.com>
2016-09-09 19:52:11 +0800

    add isIPv4Address method for string

---
+++ src/ijw.Net/StringExt.cs
@@ -0,0 +1,25 @@
+﻿namespace ijw.Net {
+    public static class StringExt {
+        public static bool IsIPv4Address(this string ip) {
+//TODO： using rex
+            string[] parts = ip.Split('.');
+            if (parts.Length != 4) {
+                return false;
+            }
+            for (int i = 0; i < parts.Length; i++) {
+                int j;
+                if (!int.TryParse(parts[i], out j)) {
+                    return false;
+                }
+                if (i == 0 && (j <= 0 || j > 255)) {
+                    return false;
+                }
+                if (i != 0 && (j < 0 || j > 255)) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
