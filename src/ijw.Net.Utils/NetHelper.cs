bd9c861c81bfc4ea441ea130d6dd5b1aea8d4ea0
jarenduan <jarenduan@gmail.com>
2016-09-09 20:44:46 +0800

    add utils project for ijw.net

---
+++ src/ijw.Net.Utils/NetHelper.cs
@@ -0,0 +1,16 @@
+﻿using ijw.Net.Html;
+namespace ijw.Net.Http.Utils
+{
+    public class NetHelper
+    {
+        /// <summary>
+        /// 使用ip.qq.com获取本机外网IP
+        /// </summary>
+        /// <returns></returns>
+        public static string GetExternalIP() {
+            string iphtml = HttpHelper.DownloadString("http://ip.qq.com/");
+            string ip = iphtml.SelectTextsByXPath("//*[@id=\"search_show\"]/span")[0];
+            return ip;
+        }
+    }
+}
