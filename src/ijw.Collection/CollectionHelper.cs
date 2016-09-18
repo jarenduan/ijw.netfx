d50436de3f8d3f50544fe68763694c7f118daba9
jarenduan <jarenduan@gmail.com>
2016-09-18 16:06:06 +0800

    remove methods return list (using toList()), remove lazy from name. edit comments.

--- src/ijw.Collection/CollectionHelper.cs
+++ src/ijw.Collection/CollectionHelper.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using ijw.Contract;
 
 namespace ijw.Collection {
 	/// <summary>
 	/// 集合操作的帮助类
 	/// </summary>
 	public static class CollectionHelper {
+		#region NewArray with values
 		/// <summary>
 		/// 创建数组，并使用指定的值填充数组
 		/// </summary>
@@ -36,53 +38,26 @@ namespace ijw.Collection {
 			}
 			return result;
 		}
+		#endregion
 
 		#region For Each Two
 		/// <summary>
-		/// 一个Helper函数，帮助需要对两个集合类对应的元素进行相同的操作。
 		/// 对两个集合进行同步迭代，对每一对元素进行操作。
-		/// 两个集合需要元素数量一致, 否则会抛出 CountNotMatchException 异常。
 		/// </summary>
 		/// <typeparam name="T1">第一个集合里面元素的类型</typeparam>
 		/// <typeparam name="T2">另一个集合里面元素的类型</typeparam>
 		/// <param name="collection1">第一个集合</param>
 		/// <param name="collection2">另一个集合</param>
 		/// <param name="doWork">需要对每对元素执行的操作</param>
-		/// <returns></returns>
+		/// <exception cref="CountNotMatchException">
+		/// 集合1元素数需要小于集合2元素数, 否则会抛出 CountNotMatchException 异常。
+		/// </exception>
 		public static void ForEachPair<T1, T2>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, Action<T1, T2> doWork) {
-			if (collection1.Count() != collection2.Count()) {
-				throw new CountNotMatchException();
-			}
-
 			IEnumerator<T2> iter = collection2.GetEnumerator();
 			foreach (var e1 in collection1) {
-				iter.MoveNext();
+				if (!iter.MoveNext()) throw new CountNotMatchException();
 				doWork(e1, iter.Current);
 			}
-		}
-
-		/// <summary>
-		/// 对两个元素数量相同的集合进行同步迭代，针对每一对元素进行指定函数计算，计算结果立即保存在一个集合中返回。
-		/// </summary>
-		/// <typeparam name="T1">第一个集合里面元素的类型</typeparam>
-		/// <typeparam name="T2">另一个集合里面元素的类型</typeparam>
-		/// <typeparam name="TResult">函数计算返回值的类型</typeparam>
-		/// <param name="collection1">第一个集合</param>
-		/// <param name="collection2">另一个集合</param>
-		/// <param name="theFunction">对每对元素执行计算的函数</param>
-		/// <returns>已经计算好的结果集合</returns>
-		public static List<TResult> ForEachPair<T1, T2, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, Func<T1, T2, TResult> theFunction) {
-			if (collection1.Count() != collection2.Count()) {
-				throw new CountNotMatchException();
-			}
-
-			var result = new List<TResult>();
-			IEnumerator<T2> iter = collection2.GetEnumerator();
-			foreach (var e1 in collection1) {
-				iter.MoveNext();
-				result.Add(theFunction(e1, iter.Current));
-			}
-			return result;
 		}
 
 		/// <summary>
@@ -96,19 +71,20 @@ namespace ijw.Collection {
 		/// <param name="theFunction">执行计算的函数</param>
 		/// <returns>返回的结果迭代器</returns>
 		/// <exception cref="CountNotMatchException">
-		/// 两个集合需要元素数量一致, 否则会抛出 CountNotMatchException 异常。
+		/// 集合1元素数需要小于集合2元素数, 否则会抛出 CountNotMatchException 异常。
 		/// </exception>
 		/// <remarks>
 		/// 本函数返回时指定函数计算并没有进行，本函数只返回一个迭代器。计算将延迟在对结果的迭代访问时进行。
 		/// </remarks>
-		public static IEnumerable<TResult> ForEachPairLazy<T1, T2, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, Func<T1, T2, TResult> theFunction) {
-			if (collection1.Count() != collection2.Count()) {
-				throw new CountNotMatchException();
-			}
+		public static IEnumerable<TResult> ForEachPair<T1, T2, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, Func<T1, T2, TResult> theFunction) {
+			//don't try to test the count(), cos' it might cause iterations.
+			//if (collection1.Count() != collection2.Count()) {
+			//	throw new CountNotMatchException();
+			//}
 
 			IEnumerator<T2> iter = collection2.GetEnumerator();
 			foreach (var e1 in collection1) {
-				iter.MoveNext();
+				if (!iter.MoveNext()) throw new CountNotMatchException(); //test during iterations.
 				yield return theFunction(e1, iter.Current);
 			}
 		}
@@ -116,11 +92,9 @@ namespace ijw.Collection {
 		#endregion
 
 		#region For Each Three
-		
+
 		/// <summary>
-		/// 一个Helper函数，帮助需要对三个集合类对应的元素进行相同的操作。
 		/// 对三个集合进行同步迭代，对每一组（三个）元素进行指定操作。
-		/// 三个集合需要元素数量一致。
 		/// </summary>
 		/// <typeparam name="T1">第一个集合里面元素的类型</typeparam>
 		/// <typeparam name="T2">第二个个集合里面元素的类型</typeparam>
@@ -129,26 +103,22 @@ namespace ijw.Collection {
 		/// <param name="collection2">另二个集合</param>
 		/// <param name="collection3">第三个集合</param>
 		/// <param name="doWork">执行的操作</param>
+		/// <exception cref="CountNotMatchException">
+		/// 集合1元素数需要小于集合2元素数, 否则会抛出 CountNotMatchException 异常。
+		/// </exception>
 		public static void ForEachThree<T1, T2, T3>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, IEnumerable<T3> collection3, Action<T1, T2, T3> doWork) {
-			if (collection1.Count() != collection2.Count() || collection2.Count() != collection3.Count()) {
-				throw new CountNotMatchException();
-			}
-
 			IEnumerator<T2> iter2 = collection2.GetEnumerator();
 			IEnumerator<T3> iter3 = collection3.GetEnumerator();
 
 			foreach (var e1 in collection1) {
-				iter2.MoveNext();
-				iter3.MoveNext();
+				if (!iter2.MoveNext()) throw new CountNotMatchException();
+				if (!iter3.MoveNext()) throw new CountNotMatchException();
 				doWork(e1, iter2.Current, iter3.Current);
 			}
 		}
 
 		/// <summary>
-		/// 一个Helper函数，帮助需要对三个集合类对应的元素进行相同的函数计算。
-		/// 对三个集合进行同步迭代，对每一组（三个）元素进行指定函数计算，计算将延迟在对结果的迭代访问时
-		/// 注意：这意味着本函数返回时，所指定的函数计算并没有真正进行，本函数只返回一个迭代器。
-		/// 三个集合需要元素数量一致。
+		/// 对三个集合进行同步迭代，对每一组（三个）元素进行指定函数计算
 		/// </summary>
 		/// <typeparam name="T1">第一个集合里面元素的类型</typeparam>
 		/// <typeparam name="T2">第二个个集合里面元素的类型</typeparam>
@@ -159,54 +129,22 @@ namespace ijw.Collection {
 		/// <param name="collection3">第三个集合</param>
 		/// <param name="theFunction">需要计算的函数</param>
 		/// <returns>返回的结果迭代器</returns>
-		public static IEnumerable<TResult> ForEachThreeLazy<T1, T2, T3, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, IEnumerable<T3> collection3, Func<T1, T2, T3, TResult> theFunction) {
-			if (collection1.Count() != collection2.Count() || collection2.Count() != collection3.Count()) {
-				throw new CountNotMatchException();
-			}
-
+		/// <exception cref="CountNotMatchException">
+		/// 集合1元素数同时需要小于集合2、3的元素数, 否则会抛出 CountNotMatchException 异常。
+		/// </exception>
+		/// <remarks>
+		/// 本函数只返回一个迭代器。计算将延迟在对结果的迭代访问时进行。
+		/// </remarks>
+		public static IEnumerable<TResult> ForEachThree<T1, T2, T3, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, IEnumerable<T3> collection3, Func<T1, T2, T3, TResult> theFunction) {
 			IEnumerator<T2> iter2 = collection2.GetEnumerator();
 			IEnumerator<T3> iter3 = collection3.GetEnumerator();
 
 			foreach (var e1 in collection1) {
-				iter2.MoveNext();
-				iter3.MoveNext();
+				if (!iter2.MoveNext()) throw new CountNotMatchException();
+				if (!iter3.MoveNext()) throw new CountNotMatchException();
 				yield return theFunction(e1, iter2.Current, iter3.Current);
 			}
 		}
-
-		/// <summary>
-		/// 一个Helper函数，帮助需要对三个集合类对应的元素进行相同的函数计算。
-		/// 对三个集合进行同步迭代，对每一组（三个）元素进行指定操作，该操作会立即迭代进行, 完成后将结果保存在一个集合中返回。
-		/// 三个集合需要元素数量一致。
-		/// </summary>
-		/// <typeparam name="T1">第一个集合里面元素的类型</typeparam>
-		/// <typeparam name="T2">第二个个集合里面元素的类型</typeparam>
-		/// <typeparam name="T3">第三个个集合里面元素的类型</typeparam>
-		/// <typeparam name="TResult">函数计算返回值得类型</typeparam>
-		/// <param name="collection1">第一个集合</param>
-		/// <param name="collection2">另二个集合</param>
-		/// <param name="collection3">第三个集合</param>
-		/// <param name="theFunction">需要计算的函数</param>
-		/// <returns>返回的已经计算好的结果集合</returns>
-		public static List<TResult> ForEachThree<T1, T2, T3, TResult>(IEnumerable<T1> collection1, IEnumerable<T2> collection2, IEnumerable<T3> collection3, Func<T1, T2, T3, TResult> theFunction) {
-			int len = collection1.Count();
-			if (!collection2.Count().Equals(len) || !collection3.Count().Equals(len)) {
-				throw new CountNotMatchException();
-			}
-			List<TResult> resultlist = new List<TResult>();
-
-			IEnumerator<T2> iter2 = collection2.GetEnumerator();
-			IEnumerator<T3> iter3 = collection3.GetEnumerator();
-
-			foreach (var e1 in collection1) {
-				iter2.MoveNext();
-				iter3.MoveNext();
-				resultlist.Add(theFunction(e1, iter2.Current, iter3.Current));
-			}
-
-			return resultlist;
-		}
-
 		#endregion
 	}
 }
