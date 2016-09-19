576eb002b07dd4b23ae188cfab830bd8e25c7588
jarenduan <jarenduan@gmail.com>
2016-09-19 15:54:39 +0800

    change parameter name

--- src/ijw.Core/StringBuilderExt.cs
+++ src/ijw.Core/StringBuilderExt.cs
@@ -8,13 +8,13 @@ namespace ijw {
         /// <summary>
         /// 移除尾部的指定字符串, 如果不符合将不更动。常用于更动字符串中的文件扩展名。
         /// </summary>
-        /// <param name="aString"></param>
+        /// <param name="sb"></param>
         /// <param name="toRemove">指定的一系列字符串，如果尾部符合，将被移除</param>
         /// <returns>移除尾部指定字符串的结果</returns>
-        public static void RemoveLast(this StringBuilder aString, params string[] toRemove) {
+        public static void RemoveLast(this StringBuilder sb, params string[] toRemove) {
             foreach (var endString in toRemove) {
-                if (aString.ToString().EndsWith(endString)) {
-                    aString.RemoveLast(endString.Length);
+                if (sb.ToString().EndsWith(endString)) {
+                    sb.RemoveLast(endString.Length);
                 }
             }
         }
@@ -22,10 +22,10 @@ namespace ijw {
         /// <summary>
         /// 从后向前删除指定数量的字符
         /// </summary>
-        /// <param name="stringBuilder"></param>
+        /// <param name="sb"></param>
         /// <param name="number">删除数量</param>
-        public static void RemoveLast(this StringBuilder stringBuilder, int number = 1) {
-            stringBuilder.Remove(stringBuilder.Length - number, number);
+        public static void RemoveLast(this StringBuilder sb, int number = 1) {
+            sb.Remove(sb.Length - number, number);
         }
     }
 }
