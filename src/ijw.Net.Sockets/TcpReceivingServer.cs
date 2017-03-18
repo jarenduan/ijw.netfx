8d795f9b18e4cf896c01771ffdc1ddbd71f0d46c
jarenduan <jarenduan@gmail.com>
2017-03-18 11:36:05 +0800

    disable warning cs4014

--- src/ijw.Net.Sockets/TcpReceivingServer.cs
+++ src/ijw.Net.Sockets/TcpReceivingServer.cs
@@ -46,6 +46,7 @@ namespace ijw.Net.Socket {
                 _logger.WriteError("[Main] Server is already running.");
                 return;
             }
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
             TaskHelper.Run(() => {
                 try {
                     _receiver.Establish();
@@ -80,6 +81,7 @@ namespace ijw.Net.Socket {
                     this._shouldContinueListen = false;
                 }
             });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
         }
 #endif
 #if !NET35 && !NET40
