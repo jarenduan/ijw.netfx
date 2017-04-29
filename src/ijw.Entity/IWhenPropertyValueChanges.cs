1f82409018bc046479e6f5c6e25609c641ae0f39
jaren@5510 <jarenduan@gmail.com>
2017-04-29 16:31:52 +0800

    add entity project and a few bases

---
+++ src/ijw.Entity/IWhenPropertyValueChanges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ijw.Entity {
+    public interface IWhenPropertyChanges {
+        event BeforePropertyChangingEventHandler BeforePropertyValueChanges;
+        event AfterPropertyChangedEventHandler AfterPropertyValueChanged;
+    }
+}
---
+++ src/ijw.Entity/PropertyValueChangingEventArgs.cs
@@ -0,0 +1,13 @@
+﻿namespace ijw.Entity {
+    public class PropertyValueChangeEventArgs {
+        public PropertyValueChangeEventArgs(string propertyName, object oldvalue, object newvalue) {
+            this.PropertyName = propertyName;
+            this.OldValue = oldvalue;
+            this.NewValue = newvalue;
+        }
+
+        public string PropertyName { get; protected set; }
+        public object OldValue { get; protected set; }
+        public object NewValue { get; protected set; }
+    }
+}
