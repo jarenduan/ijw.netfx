8536246e5cd1ba2bb8e7b85fa923640683fae6e8
jarenduan <jarenduan@gmail.com>
2017-12-19 19:37:24 +0800

    升级为跨平台，整理using

--- src/ijw.Net.Utils/NetHelper.cs
+++ src/ijw.Net.Utils/NetHelper.cs
@@ -1,5 +1,7 @@
-﻿using ijw.Net.Html;
+﻿#if !NET35
+using ijw.Net.Html;
 using ijw.Net.Http;
+using ijw.Text;
 
 namespace ijw.Net.Utils
 {
@@ -10,9 +12,10 @@ namespace ijw.Net.Utils
         /// </summary>
         /// <returns></returns>
         public static string GetExternalIP() {
-            string iphtml = HttpHelper.DownloadString("http://ip.qq.com/");
+            string iphtml = HttpHelper.DownloadString("http://ip.qq.com/", EncodingHelper.GB2312);
             string ip = iphtml.SelectTextsByXPath("//*[@id=\"search_show\"]/span")[0];
             return ip;
         }
     }
 }
+#endif
