2281843403778ee68e78fa5adb97b4c216d5495b
jarenduan <jarenduan@gmail.com>
2017-12-28 17:03:22 +0800

    完善函数重载

--- src/ijw.Net.Http/HttpHelper.cs
+++ src/ijw.Net.Http/HttpHelper.cs
@@ -1,7 +1,8 @@
-﻿//#if NET35 || NET40 || NET45
+﻿#if !NET35
 using ijw.Diagnostic;
 using ijw.IO;
 using ijw.Text;
+using ijw.Contract;
 using System;
 using System.IO;
 using System.Net;
@@ -13,18 +14,7 @@ namespace ijw.Net.Http {
     /// </summary>
     public static partial class HttpHelper {
         /// <summary>
-        /// 从指定url下载字符串, 使用GB2312编码。
-        /// </summary>
-        /// <param name="url">url地址</param>
-        /// <param name="userAgent">浏览器类型，默认是Firefox</param>
-        /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
-        /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
-        /// <returns>下载的字符串</returns>
-        public static string DownloadString(string url, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10) {
-            return DownloadString(url, EncodingHelper.GB2312, userAgent, connectTimeout, readTimeout);
-        }
-        /// <summary>
-        /// 使用指定的编码从指定url下载字符串, 。
+        /// 使用指定的编码从指定url下载字符串,。
         /// </summary>
         /// <param name="url">url地址</param>
         /// <param name="encoding">编码方式</param>
@@ -32,30 +22,33 @@ namespace ijw.Net.Http {
         /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
         /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
         /// <returns>下载的字符串</returns>
-        public static string DownloadString(string url, Encoding encoding, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10) {
+        public static string DownloadString(string url,
+                                           Encoding encoding = null,
+                                           string userAgent = BrowserUserAgent.Firefox,
+                                           int connectTimeout = 1000 * 10,
+                                           int readTimeout = 1000 * 10,
+                                           bool ifKeepAlive = true,
+                                           string referer = null,
+                                           CookieContainer oldCookies = null,
+                                           string accept = null,
+                                           string accept_encoding = null,
+                                           string cache_control = null) {
             return GetWebResponseContent(
                 url,
-                (stream) => stream.ReadStringAndDispose(encoding),
+                (stream) => stream.ReadStringAndDispose(encoding ?? Encoding.Unicode),
                 userAgent,
                 connectTimeout,
-                readTimeout);
+                readTimeout,
+                ifKeepAlive,
+                referer,
+                oldCookies,
+                accept,
+                accept_encoding,
+                cache_control).result;
         }
-
+              
         /// <summary>
-        /// 使用GB2312编码方式从指定的URL下载文本数据, 并按utf-8编码方式存为指定文件
-        /// </summary>
-        /// <param name="url">指定的url</param>
-        /// <param name="filename">指定的文件名</param>
-        /// <param name="userAgent">浏览器类型, 默认是Firefox</param>
-        /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
-        /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
-        /// <param name="append">是否是追加模式, true为追加, false为新建或覆盖, 默认是false</param>
-        /// <returns>下载的字符串长度</returns>
-        public static long DownloadStringToFile(string url, string filename, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10, bool append = false) {
-            return DownloadStringToFile(url, filename, EncodingHelper.GB2312, Encoding.UTF8, userAgent, connectTimeout, readTimeout, append);
-        }
-        /// <summary>
-        /// 使用指定的编码方式从指定的URL下载文本数据, 并按指定的编码方式和文件名存为文本文件
+        /// 使用指定的编码方式从指定的URL下载文本数据, 并按指定的编码方式和文件名存为文本文件.
         /// </summary>
         /// <param name="url">指定的url</param>
         /// <param name="filename">指定的文件名</param>
@@ -66,29 +59,39 @@ namespace ijw.Net.Http {
         /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
         /// <param name="append">是否是追加模式, true为追加, false为新建或覆盖, 默认是false</param>
         /// <returns>下载的字符串长度</returns>
-        public static long DownloadStringToFile(string url, string filename, Encoding readEncoding, Encoding writeEncoding, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10, bool append = false) {
+        public static int DownloadStringToFile(string url,
+                                               string filename,
+                                               Encoding readEncoding = null,
+                                               Encoding writeEncoding = null,
+                                               string userAgent = BrowserUserAgent.Firefox,
+                                               int connectTimeout = 1000 * 10,
+                                               int readTimeout = 1000 * 10,
+                                               bool ifKeepAlive = true,
+                                               string referer = null,
+                                               CookieContainer oldCookies = null,
+                                               string accept = null,
+                                               string accept_encoding = null,
+                                               string cache_control = null,
+                                               bool append = false) {
+            
             return GetWebResponseContent(
                 url,
-                (stream) => {
-                    stream.WriteToTextFileAndDispose(filename, readEncoding, writeEncoding, append);
-                    return 0;
-                },
+                (stream) =>stream.WriteToTextFileAndDispose(filename, 
+                                                            readEncoding ?? Encoding.Unicode, 
+                                                            writeEncoding ?? Encoding.Unicode, 
+                                                            append),
                 userAgent,
                 connectTimeout,
-                readTimeout);
+                readTimeout,
+                ifKeepAlive,
+                referer,
+                oldCookies,
+                accept,
+                accept_encoding,
+                cache_control).result;
         }
 
-        /// <summary>
-        /// 使用UTF-8编码从指定的URL下载全部二进制数据
-        /// </summary>
-        /// <param name="url">url地址</param>
-        /// <param name="userAgent">浏览器类型, 默认是Firefox</param>
-        /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
-        /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
-        /// <returns>下载的二进制数据</returns>
-        public static byte[] DownloadBytes(string url, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10) {
-            return DownloadBytes(url, Encoding.UTF8, userAgent, connectTimeout, readTimeout);
-        }
+     
         /// <summary>
         /// 使用指定编码从指定的URL下载全部二进制数据
         /// </summary>
@@ -98,28 +101,32 @@ namespace ijw.Net.Http {
         /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
         /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
         /// <returns>下载的二进制数据</returns>
-        public static byte[] DownloadBytes(string url, Encoding encoding, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10) {
+        public static byte[] DownloadBytes(string url,
+                                           Encoding encoding = null, 
+                                           string userAgent = BrowserUserAgent.Firefox, 
+                                           int connectTimeout = 1000 * 10, 
+                                           int readTimeout = 1000 * 10,
+                                           bool ifKeepAlive = true,
+                                           string referer = null,
+                                           CookieContainer oldCookies = null,
+                                           string accept = null,
+                                           string accept_encoding = null,
+                                           string cache_control = null) {
             return GetWebResponseContent(
                 url,
-                (stream) => stream.ReadBytesAndDispose(),
+                (stream) => stream.ReadBytesAndDispose(encoding ?? Encoding.Unicode),
                 userAgent,
                 connectTimeout,
-                readTimeout);
+                readTimeout,
+                ifKeepAlive, 
+                referer, 
+                oldCookies, 
+                accept, 
+                accept_encoding, 
+                cache_control).result;
         }
 
-        /// <summary>
-        /// 使用Unicode编码从指定的URL下载二进制数据, 并使用Unicode编码存入指定文件
-        /// </summary>
-        /// <param name="url">指定的url</param>
-        /// <param name="filename">指定的文件名</param>
-        /// <param name="userAgent">浏览器类型, 默认是Firefox</param>
-        /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
-        /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
-        /// <param name="append">是否是追加模式, true为追加, false为新建或覆盖, 默认是false</param>
-        /// <returns>下载数据的长度</returns>
-        public static long DownloadBytesToFile(string url, string filename, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10, bool append = false) {
-            return DownloadBytesToFile(url, filename, Encoding.Unicode, Encoding.Unicode, userAgent, connectTimeout, readTimeout, append);
-        }
+    
         /// <summary>
         /// 使用指定编码方式从指定的URL下载二进制数据, 并按指定的编码方式存入指定文件
         /// </summary>
@@ -132,13 +139,35 @@ namespace ijw.Net.Http {
         /// <param name="readTimeout">读取网络流超时时间, 默认是10秒钟</param>
         /// <param name="append">写入文件时是否采用追加模式, true为追加, false为新建或覆盖, 默认是false</param>
         /// <returns>下载数据的长度</returns>
-        public static long DownloadBytesToFile(string url, string filename, Encoding readEncoding, Encoding writeEncoding, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10, bool append = false) {
+        public static long DownloadBytesToFile(string url, 
+                                               string filename, 
+                                               Encoding readEncoding = null, 
+                                               Encoding writeEncoding = null,
+                                               string userAgent = BrowserUserAgent.Firefox,
+                                               int connectTimeout = 1000 * 10,
+                                               int readTimeout = 1000 * 10,
+                                               bool ifKeepAlive = true,
+                                               string referer = null,
+                                               CookieContainer oldCookies = null,
+                                               string accept = null,
+                                               string accept_encoding = null,
+                                               string cache_control = null,
+                                               bool append = false) {
             return GetWebResponseContent(
                 url,
-                (stream) => stream.WriteToBinaryFileAndDispose(filename, readEncoding, writeEncoding, append),
+                (stream) => stream.WriteToBinaryFileAndDispose(filename, 
+                                                                readEncoding ?? Encoding.Unicode, 
+                                                                writeEncoding ?? Encoding.Unicode, 
+                                                                append),
                 userAgent,
                 connectTimeout,
-                readTimeout);
+                readTimeout,
+                ifKeepAlive,
+                referer,
+                oldCookies,
+                accept,
+                accept_encoding,
+                cache_control).result;
         }
 
         /// <summary>
@@ -151,19 +180,48 @@ namespace ijw.Net.Http {
         /// <param name="connectTimeout">连接超时时间, 默认是10秒钟</param>
         /// <param name="readTimeout">读取Response流超时时间, 默认是10秒钟</param>
         /// <returns>处理后的内容</returns>
-        public static T GetWebResponseContent<T>(string url, Func<Stream, T> func, string userAgent = BrowserUserAgent.Firefox, int connectTimeout = 1000 * 10, int readTimeout = 1000 * 10) {
+        public static (T result, CookieContainer cookies) GetWebResponseContent<T>(
+            string url,
+            Func<Stream, T> func,
+            string userAgent = BrowserUserAgent.Firefox,
+            int connectTimeout = 1000 * 10,
+            int readTimeout = 1000 * 10,
+            bool ifKeepAlive = true,
+            string referer = null,
+            CookieContainer oldCookies = null,
+            string accept = null,
+            string accept_encoding = null,
+            string cache_control = null)
+        {
+            url.ShouldBeNotNullArgument();
+            func.ShouldBeNotNullArgument();
+
             HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
             request.UserAgent = userAgent;
             request.Timeout = connectTimeout;
             request.ReadWriteTimeout = readTimeout;
             request.Method = "GET";
+            request.KeepAlive = ifKeepAlive;
+            request.Referer = referer ?? url;
+            if (accept != null) {
+                request.Accept = accept;
+            }
+            if (accept_encoding != null) {
+                //request.SendChunked = true;
+                //request.TransferEncoding = accept_encoding;
+            }
+            if (oldCookies != null) {
+                request.CookieContainer = oldCookies;
+            }
             HttpWebResponse response = null;
             try {
                 response = request.GetResponse() as HttpWebResponse;
                 Stream receiveStream = response.GetResponseStream();
                 T content = func(receiveStream);
                 DebugHelper.WriteLine(content.ToString());
-                return content;
+                response.Close();
+                CookieContainer cookies = request.CookieContainer;
+                return (content, cookies);
             }
             catch {
                 throw;
@@ -175,4 +233,4 @@ namespace ijw.Net.Http {
         }
     }
 }
-//#endif
+#endif
