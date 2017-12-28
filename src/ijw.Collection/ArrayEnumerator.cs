4bff1ab239e7070eb0bae4aecd643cf69ccc5b82
jarenduan <jarenduan@gmail.com>
2017-12-28 17:02:11 +0800

    清理代码

--- src/ijw.Collection/ArrayEnumerator.cs
+++ src/ijw.Collection/ArrayEnumerator.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace ijw.Collection {
-   
 
     public class ArrayEnumerator<T> : IEnumerator<T> {
         protected T[] _data;
@@ -13,19 +12,23 @@ namespace ijw.Collection {
         }
 
         private int _curr = -1;
+
         public T Current {
             get { return this._data[_curr]; }
         }
+
         public void Dispose() {
         }
 
         object IEnumerator.Current {
             get { return this.Current; }
         }
+
         public bool MoveNext() {
             this._curr++;
             return (this._curr < this._data.Length);
         }
+
         public void Reset() {
             this._curr = -1;
         }
