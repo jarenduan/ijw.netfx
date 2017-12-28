26a4431acb58a907e79c9e95c71d774087faeb50
jarenduan <jarenduan@gmail.com>
2017-12-28 16:59:37 +0800

    增加bytes write to file的扩展方法

---
+++ src/ijw.IO/BytesExt.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace ijw.IO {
+    public static class BytesExt {
+        public static void WriteToFile(this byte[] content, string filename, bool append = false) {
+            content.WriteToFile(filename, Encoding.Unicode);
+        }
+
+        public static void WriteToFile(this byte[] content, string filename, Encoding encoding, bool append = false) {
+            FileMode filemode = append ? FileMode.Append : FileMode.Create;
+            FileStream file = new FileStream(filename, filemode);
+            using (BinaryWriter writer = new BinaryWriter(file, encoding)) {
+                writer.Write(content);
+                writer.Flush();
+            }
+        }
+    }
+}
